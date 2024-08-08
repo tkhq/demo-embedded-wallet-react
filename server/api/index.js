07ce0f0e2b0b28f44872bb30ab9e2e52f4ab1f85
Andrew Min <andrew@turnkey.io>
2024-08-08 16:46:24 -0700

    Revert "dedupe server"
    This reverts commit 1d325d41c0f22ab606d43bbe2d05095e2e3cb8bf.

--- server/api/index.js
+++ server/api/index.js
@@ -4,17 +4,17 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5858;
 const fs = require("fs");
-require("dotenv").config();
+require('dotenv').config();
 
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_PRIVATE_KEY = process.env.API_PRIVATE_KEY;
 const API_PUBLIC_KEY = process.env.API_PUBLIC_KEY;
 const DEFAULT_ORGANIZATION_ID = process.env.DEFAULT_ORGANIZATION_ID;
 
-const { addUser, findUserByEmail, verifyUserEmail } = require("./database");
+const { addUser, findUserByEmail, verifyUserEmail } = require('../database');
 
 const corsOptions = {
-  origin: "*",
+  origin: '*'
 };
 
 app.use(cors(corsOptions));
@@ -24,8 +24,8 @@ const turnkeyConfig = {
   apiBaseUrl: API_BASE_URL,
   apiPrivateKey: API_PRIVATE_KEY,
   apiPublicKey: API_PUBLIC_KEY,
-  defaultOrganizationId: DEFAULT_ORGANIZATION_ID,
-};
+  defaultOrganizationId: DEFAULT_ORGANIZATION_ID
+}
 
 const turnkeyServerClient = new Turnkey(turnkeyConfig);
 
@@ -37,57 +37,31 @@ const turnkeyProxyHandler = turnkeyServerClient.expressProxyHandler({
     "initUserEmailRecovery",
     "initImportWallet",
     "initImportPrivateKey",
-    "getSubOrgIds",
-  ],
+    "getSubOrgIds"
+  ]
 });
 
-app.get("/", (req, res) =>
-  res.send("Welcome to Turnkey's Demo Embedded Wallet API!"),
-);
+app.get("/", (req, res) => res.send('Express on Vercel'));
 
 app.post("/", turnkeyProxyHandler);
 
 app.post("/add-user", async (req, res) => {
-  try {
-    const addUserResponse = await addUser(
-      req.body.email,
-      req.body.subOrganizationId,
-      0,
-    );
-    res.status(200).send({ user: addUserResponse });
-  } catch (error) {
-    res.status(500).send({ error: `Failed to add user: ${error}` });
-  }
+  const addUserResponse = await addUser(req.body.email, req.body.subOrganizationId, 0);
+  res.status(200).send({ user: addUserResponse });
 });
 
 app.post("/find-user-by-email", async (req, res) => {
-  if (!req.body.email) {
-    res.status(400).send({ error: "No email address provided." });
-  }
-
-  try {
-    const user = await findUserByEmail(req.body.email);
-    res.status(200).send({ user: user });
-  } catch (error) {
-    res
-      .status(400)
-      .send({ error: `User with email ${req.body.email} not found: ${error}` });
-  }
+  const user = await findUserByEmail(req.body.email);
+  res.status(200).send({ user: user });
 });
 
 app.post("/verify-user-email", async (req, res) => {
-  try {
-    const verifyUserResponse = await verifyUserEmail(req.body.email);
-    res.status(200).send({ user: verifyUserResponse });
-  } catch (error) {
-    res.status(400).send({
-      error: `Unable to verify user email ${req.body.email}: ${error}`,
-    });
-  }
+  const verifyUserResponse = await verifyUserEmail(req.body.email);
+  res.status(200).send({ user: verifyUserResponse });
 });
 
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// FORCE VERCEL REDEPLOY
+module.exports = app;
