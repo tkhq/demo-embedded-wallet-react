9b9f71d9c606fce274246164e4ca4342f22633dd
Andrew Min <andrew@turnkey.io>
2024-08-08 16:45:34 -0700

    Revert "fixing vercel api"
    This reverts commit 94397eada52f60a60e99b2bd4287321a2c76578a.

--- server/api/server.js
+++ server/api/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5858;
+const fs = require("fs");
 require("dotenv").config();
 
 const API_BASE_URL = process.env.API_BASE_URL;
