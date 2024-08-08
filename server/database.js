2f8430526b06a138ed2ff9d1ad55e0c2303efc8f
Andrew Min <andrew@turnkey.io>
2024-08-08 16:45:48 -0700

    Revert "nest server"
    This reverts commit a78071efde73ae6b22c4b14cd316f909fbbd2504.

--- server/database.js
+++ server/database.js
@@ -4,8 +4,6 @@ if (process.env.NODE_ENV === "production") {
   database = require("./postgres");
 } else if (process.env.NODE_ENV === "development") {
   database = require("./sqlite");
-} else {
-  throw new Error("NODE_ENV must be set");
 }
 
 module.exports = database;
